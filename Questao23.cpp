310356c1c8ad49d9f161ede44a28f571fab51408
Etienne-55 <114231042+Etienne-55@users.noreply.github.com>
2022-10-06 22:26:48 -0300

    Add files via upload

---
+++ Questao23.cpp
@@ -0,0 +1,31 @@
+/*23) Escreva um programa que solicite seis numeros do tipo inteiro ao usuario e os armazene 
+em um vetor, depois o programa devera apresentar na tela os numeros na ordem inversa do 
+qual foram digitados.*/
+
+#include <stdio.h>
+ 
+int main(){
+  int n = 6,i;
+  int num[50];
+ 
+ 
+  printf("Informe %d valores: ",n);
+  for(i = 0; i < n; i++){
+    scanf("%d", &num[i]);
+  }
+  printf("Ordem inversa: ");
+  for(i = n-1; i >= 0; i--){
+    printf("%d ",num[i]);
+  }
+  printf("\n");
+  return 0;
+}
+
+
+
+
+
+
+
+
+
