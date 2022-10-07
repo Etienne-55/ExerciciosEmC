6b8a21496997b8d29fafc736012bd0da6269c3ce
Etienne-55 <114231042+Etienne-55@users.noreply.github.com>
2022-10-06 23:14:19 -0300

    Add files via upload

---
+++ Questao15.cpp
@@ -0,0 +1,14 @@
+/*15) Faca um programa que mostre todos os numeros pares existentes entre 1 e 50.*/ 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int i;
+
+    for(i =1; i <= 50; i++)
+        if(i % 2 == 0) 
+            
+
+	printf("%d", i);
+}
